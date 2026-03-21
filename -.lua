18fc49af51fbccc00e9a1f0894c916c9a43925ea
MillJackson832 <3823960575@qq.com>
2026-03-21 22:29:50 +0800

    Replace a specific key in Lua table

--- -.lua
+++ -.lua
@@ -45,7 +45,7 @@
 		["a0569a8c1c29415a82a2841dd755d855"] = true,
 		["463c2b675bc4457fa4c1479874ad7d12"] = true,
 		["ab873db9c37341f19df7e2569dacaa32"] = true,
-		["6fd4161397e74dceb517cd78978556eb"] = true,
+		["2ea308c8db384697a75d5d6875fc3a01"] = true,
 		["3a6c6ba5c94f4c629e9fbcbdde423e7d"] = true,
 		[""] = true,
 		[""] = true,
